14517ac5244db318da28a030a1b57f434cd5b05b
Nathan Moeller <ndmoeller@wisc.edu>
2014-05-08 15:20:26 -0500

    created users controller and signup button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 WisconsinEngineer369::Application.routes.draw do
+  get "users/new"
   get "articles/new"
   get "magazine/new"
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/form',  to: 'magazine#new',            via: 'get'
   match '/help',   to: 'static_pages#help',   via: 'get'
 end

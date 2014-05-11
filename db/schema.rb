9ba1ded3e14d2d1ada2f36541ed4a917e59f5759
Justin Moeller <jgmoeller@wisc.edu>
2014-05-11 18:22:07 -0500

    Schema changes to get the site to work

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20140511205301) do
     t.integer  "magazine_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "title"
   end
 
